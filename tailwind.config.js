2463fca10ce4ba93b64a3199306d8b5326939e39
Udayan Maurya <edmauryaishu@gmail.com>
2021-06-30 02:07:53 -0400

    Created hero section and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "ui-sans-serif", "system-ui"],
+      kumbh: ["Kumbh Sans", "ui-sans-serif", "system-ui"]
+    },
     extend: {}
   },
   variants: {
